903759f488296104b7d097b9fa8dfa53ffe2f021
errih anes <anes.errih@etu.univ-lyon1.fr>
2025-06-15 00:14:10 +0200

    modification du text

--- src/components/welcomeCard.tsx
+++ src/components/welcomeCard.tsx
@@ -10,8 +10,8 @@ const WelcomeCard = () => {
                 <div className="w-full">
                     <h2 className="text-sm text-gray-400 mb-2">welcome</h2>
                     <h3 className="text-sm sm:text-lg font-semibold mb-4">Bonjour, </h3>
-                    <p className="text-gray-300 text-sm leading-relaxed">Étudiant en informatique diplômé de l’Université Lyon 1, je cherche une alternance en master développement web, logiciel, data science ou IA. Passionné par les nouvelles technologies, je souhaite allier pratique et apprentissage continu.</p>
-
+                    <p className="text-gray-300 text-sm leading-relaxed">Étudiant en informatique diplômé de l’Université Lyon 1, je cherche une alternance en master développement web, logiciel, data science ou IA. je souhaite allier pratique et apprentissage continu.</p>
+                    <p className="text-gray-300 text-sm leading-relaxed mt-4 mb-2">N’hésitez pas à me contacter si vous avez une opportunité d’alternance ou un projet à me proposer.</p>
                 </div>
                 <div>
                     <img src={avatar} alt="avatar" className="w-24 h-24 sm:w-32 sm:h-32 object-contain rounded-full border" />
